996a15faa5d921fc5864636aaa76b08527524904
giulio93 <pilotto.giulio@gmail.com>
2019-07-23 15:37:56 +0200

    just a safe save

--- route.py
+++ route.py
@@ -3,8 +3,8 @@ class Route:
 
     def __init__(self,capacity):
         self.capacity = capacity,
-        self.customer = []
-        self.weight = 0,
+        self.customers = []
+        self.payload = 0,
         self.cost = 0,
         self.savings =0
 
@@ -14,37 +14,45 @@ class Route:
             print("Customer demand too large")
             return -1
         else:    
-                self.weight += demand
-                if(self.weight >  self.capacity):
+                tempPayload = self.payload + demand
+                if(tempPayload >  self.capacity):
                     print("Route Overloaded")
                     return -1
                 else:
+                    self.payload = tempPayload
                     if onTop == True :
-                        self.customer.insert(0,index)
+                        self.customers.insert(0,index)
                         return 1
                     else:
-                        self.customer.insert(len(self.customer),index)
+                        self.customers.insert(len(self.customers),index)
                         return 1
 
         
     
     def checkCustomer(self, index):
-        if index not in self.customer:
+        if index not in self.customers:
             print("Customer: " + str(index) +" not present")
             return -1
            
         else:
 
-            i = self.customer.index(index)
-            if (0<i<len(self.customer)-1):
+            i = self.customers.index(index)
+            if (0<i<len(self.customers)-1):
                 print("Customer: " + str(index) +" interior in the tour")
                 return -2
 
             else: 
                 return i #Yeah, it's the starting point or the end point
     
+    def getCustomers(self):
+        return self.customers
+
+    def getPayload(self):
+        return self.payload
+     
+
     def printRoute(self):
         print("Depot ")
-        for c in  self.customer:
+        for c in  self.customers:
             print(" - " + c )
         print("Depot")
--- solver.py
+++ solver.py
@@ -26,6 +26,8 @@ def ClarkeWright(graph):
     for save in savings:
         i = save[1]
         j = save[2]
+        customerI = -1
+        customerJ = -1
 
         # no routes have been created yet
         if len(routes) == 0:
@@ -36,15 +38,17 @@ def ClarkeWright(graph):
         else:
             #check if customer i and j have been already served
             for route in routes:
-                customerI = route.checkCustomer(i)
-                customerJ = route.checkCustomer(j)
+                if(-2< customerI < 0):
+                    customerI = route.checkCustomer(i)
+                if(-2< customerJ < 0):
+                    customerJ = route.checkCustomer(j)
                 #check if i and j have been served by a single route
-                if(customerI != -1):
+                if(customerI >= 0 and customerJ == -1):
                     routeA = route
-                elif(customerJ != -1):
+                elif(customerJ >=0 and customerI ==  -1):
                     routeB = route
             # case: i and j have not been served yet
-            if(customerI == -1  and customerJ == -1):
+            if(-2 < customerI < 0  and -2 < customerJ < 0):
                 r =  Route(graph.getCapacity())
                 control =  r.addCustomer(i,demand[i],False)
                 control =  r.addCustomer(j,demand[i],False)
@@ -52,37 +56,38 @@ def ClarkeWright(graph):
                 if(control != -1):
                     routes.append(r)
             #customer i is served but j not.
-            if (customerI != -1 and customerJ == -1):
+            if (customerI >= 0 and customerJ == -1):
                 if(customerI == 0):
                     route.addCustomer(j,demand[j],True)
                 else:
                     route.addCustomer(j,demand[j],False)
             #customer j is served but i not.
-            if (customerI == -1 and customerJ != -1):
+            if (customerI == -1 and customerJ >= 0):
                 if(customerJ == 0):
                   route.addCustomer(i,demand[i],True)
                 else:
                   route.addCustomer(i,demand[i],False)
 
             # both i and j have been served already, but from different routes, MERGE them
-            if (customerI != -1 and customerJ != -1 and routeA != -1 and routeB!= -1) :
-                if (routeA != routeB):
-                
+            if (customerI >= 0 and customerJ >= 0 and routeA != -1 and routeB!= -1) :
+                if (routes.index(routeA) != routes.index(routeB)):
+                    # Does routeA and routeB merge's overload final route
+                    if(routeA.getPayload() + routeB.getPayload() <= graph.getCapacity()):
                         #The edge that connect routeA to routeB resides in their heads
                         if(customerI == 0 and customerJ == 0):                     
-                            for customer in routeA:
+                            for customer in routeA.getCustomers():
                                 routeB.addCustomer(customer,demand[customer],True)
                         #The edge that connect routeA to routeB resides in head of routeA and tail of routeB
-                        if(customerI == 0 and customerJ != 0):                     
-                            for customer in routeA:
+                        if(customerI == 0 and customerJ > 0):                     
+                            for customer in routeA.getCustomers():
                                 routeB.addCustomer(customer,demand[customer],False)
                         #The edge that connect routeA to routeB resides in head of routeB and tail of routeA
-                        if(customerI != 0 and customerJ == 0):                     
-                            for customer in routeB:
+                        if(customerI > 0 and customerJ == 0):                     
+                            for customer in routeB.getCustomers():
                                 routeA.addCustomer(customer,demand[customer],False)
                         #The edge that connect routeA to routeB resides in their tails
-                        if(customerI != 0 and customerJ != 0):                     
-                            for customer in routeB:
+                        if(customerI > 0 and customerJ > 0):                     
+                            for customer in reversed(routeB.getCustomers()):
                                 routeA.addCustomer(customer,demand[customer],False)
                             
         #filize routing adding connection to the Depot
