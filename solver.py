d1e11695f524edc2de2398f7e7fe35100a12f951
giulio93 <pilotto.giulio@gmail.com>
2019-08-09 13:13:26 +0200

    big fix in CW

--- solver.py
+++ solver.py
@@ -83,10 +83,10 @@ def ClarkeWright(graph):
     savings.sort(key=lambda x: x[0], reverse=True)
 
     routes = []
-    # for x in range(1, dimension):
-    #     appoRoute =  Route(graph.getCapacity()) 
-    #     appoRoute.addCustomer(x,demand[x],False)
-    #     routes.append(appoRoute)
+    for x in range(1, dimension):
+        appoRoute =  Route(graph.getCapacity()) 
+        appoRoute.addCustomer(x,demand[x],False)
+        routes.append(appoRoute)
 
     for save in savings:
         i = save[1]
@@ -95,76 +95,58 @@ def ClarkeWright(graph):
         customerJ = -1
         routeA = -1
         routeB = -1
+        routeSelected = -1
 
         # no routes have been created yet
-        if len(routes) == 0:
-            firstRoute =  Route(graph.getCapacity())
-            firstRoute.addCustomer(i,demand[i],True)
-            firstRoute.addCustomer(j,demand[j],False)
-            routes.append(firstRoute)
-        else:
+        # if len(routes) == 0:
+        #     firstRoute =  Route(graph.getCapacity())
+        #     firstRoute.addCustomer(i,demand[i],True)
+        #     firstRoute.addCustomer(j,demand[j],False)
+        #     routes.append(firstRoute)
+        
             #check if customer i and j have been already served
-            for route in routes:
-                if(-2< customerI < 0):
-                    customerI = route.checkCustomer(i)
-                    routeA = route
-                if(-2< customerJ < 0):
-                    customerJ = route.checkCustomer(j)
-                    routeB = route
-                #check if i and j have been served by a single route
-                  
-            # case: i and j have not been served yet
-            if(-2 < customerI < 0  and -2 < customerJ < 0):
-                newRoute =  Route(graph.getCapacity())
-                control1 =  newRoute.addCustomer(i,demand[i],False)
-                control2 =  newRoute.addCustomer(j,demand[j],False)
-                # No constraint violated on the route
-                if(control1 != -1 and control2 != -1):
-                    routes.append(newRoute)
-            #customer i is served but j not.
-            if (customerI >= 0 and customerJ == -1):
-                if(customerI == 0):
-                    route.addCustomer(j,demand[j],True)
-                else:
-                    route.addCustomer(j,demand[j],False)
-            #customer j is served but i not.
-            if (customerI == -1 and customerJ >= 0):
-                if(customerJ == 0):
-                  route.addCustomer(i,demand[i],True)
-                else:
-                  route.addCustomer(i,demand[i],False)
+        for route in routes:
+            if(-2< customerI < 0):
+                customerI = route.checkCustomer(i)
+                routeA = route
+                routeSelected = route
+            if(-2< customerJ < 0):
+                customerJ = route.checkCustomer(j)
+                routeB = route
+                routeSelected = route
+            #check if i and j have been served by a single route
+            
+            #routes = Sequential_CW(routeSelected,routes,customerI,customerJ,i,j,demand)
 
             # both i and j have been served already, but from different routes, MERGE them
-            if (customerI >= 0 and customerJ >= 0 and routeA != -1 and routeB!= -1) :
-                if (routes.index(routeA) != routes.index(routeB)):
-                    # Does routeA and routeB merge's overload final route
-                    if(routeA.getPayload() + routeB.getPayload() <= graph.getCapacity()):
-                        #The edge that connect routeA to routeB resides in their heads
-                        if(customerI == 0 and customerJ == 0):                     
-                            for customer in routeA.getCustomers():
-                                routeB.addCustomer(customer,demand[customer],True)
-                            routes.remove(routeA)
-                        
-                        #The edge that connect routeA to routeB resides in head of routeA and tail of routeB
-                        if(customerI == 0 and customerJ > 0):                     
-                            for customer in routeA.getCustomers():
-                                routeB.addCustomer(customer,demand[customer],False)
-                            routes.remove(routeA)
-       
-                        #The edge that connect routeA to routeB resides in head of routeB and tail of routeA
-                        if(customerI > 0 and customerJ == 0):                     
-                            for customer in routeB.getCustomers():
-                                routeA.addCustomer(customer,demand[customer],False)
-                            routes.remove(routeB)
-
-                        #The edge that connect routeA to routeB resides in their tails
-                        if(customerI > 0 and customerJ > 0):                     
-                            for customer in reversed(routeB.getCustomers()):
-                                routeA.addCustomer(customer,demand[customer],False)
-                            routes.remove(routeB)
-
-            print("Savings number :" + str(savings.index(save)))    
-            #savings.remove(save) 
+        if (customerI >= 0 and customerJ >= 0 and routeA != -1 and routeB!= -1) :
+            if (routes.index(routeA) != routes.index(routeB)):
+                # Does routeA and routeB merge's overload final route
+                if(routeA.getPayload() + routeB.getPayload() <= graph.getCapacity()):
+                    #The edge that connect routeA to routeB resides in their heads
+                    if(customerI == 0 and customerJ == 0):                     
+                        for customer in routeA.getCustomers():
+                            routeB.addCustomer(customer,demand[customer],True)
+                        routes.remove(routeA)
+                    
+                    #The edge that connect routeA to routeB resides in head of routeA and tail of routeB
+                    if(customerI == 0 and customerJ > 0):                     
+                        for customer in routeA.getCustomers():
+                            routeB.addCustomer(customer,demand[customer],False)
+                        routes.remove(routeA)
+    
+                    #The edge that connect routeA to routeB resides in head of routeB and tail of routeA
+                    if(customerI > 0 and customerJ == 0):                     
+                        for customer in routeB.getCustomers():
+                            routeA.addCustomer(customer,demand[customer],False)
+                        routes.remove(routeB)
+                    #The edge that connect routeA to routeB resides in their tails
+                    if(customerI > 0 and customerJ > 0):                     
+                        for customer in reversed(routeB.getCustomers()):
+                            routeA.addCustomer(customer,demand[customer],False)
+                        routes.remove(routeB)
+        print("Savings number :" + str(savings.index(save)))    
+        #savings.remove(save) 
     
     #If a node is has not been served yet, let's add a route just for it
     for node in range(1,graph.getDimension() -1):
@@ -181,26 +163,30 @@ def ClarkeWright(graph):
         fianlRoute.addCustomer(0,0,True)
         fianlRoute.addCustomer(0,0,False)
 
-   
-    #Finile routing adding connection to the Depot, print Route path and cost in a file
-    # routeCost = 0  
-    # routedNodesControl = 1
-    # f= open("mysol/Sol_"+graph.getFileName()+".txt","w+")
-    # f.write(str(graph.name)+"\n")
-    # f.write(str(graph.dimension)+"\n")
-    
-    # for fianlRoute in routes:
-    #     fianlRoute.addCustomer(0,0,True)
-    #     fianlRoute.addCustomer(0,0,False)        
-    #     appo = fianlRoute.printRoute(routes.index(fianlRoute))
-    #     f.write(appo+"\n")
-    #     for i in range(len(fianlRoute.getCustomers())-1):
-    #         routedNodesControl = routedNodesControl +1
-    #         routeCost += graph.getValue(fianlRoute.getCustomers()[i], fianlRoute.getCustomers()[i+1])
-    #     routedNodesControl = routedNodesControl -1      
-    # f.write("Total Routed Nodes "+ str(routedNodesControl)+"\n")
-    # f.write("Routing Total Cost: "+ str(routeCost)+"\n")
+    return routes
 
+def Sequential_CW(routeSelected,routes,customerI,customerJ,i,j,demand,graph):
+     # case: i and j have not been served yet
+    if(-2 < customerI < 0  and -2 < customerJ < 0):
+        newRoute =  Route(graph.getCapacity())
+        control1 =  newRoute.addCustomer(i,demand[i],False)
+        control2 =  newRoute.addCustomer(j,demand[j],False)
+        # No constraint violated on the route
+        if(control1 != -1 and control2 != -1):
+            routes.append(newRoute)
+    #customer i is served but j not.
+    if (customerI >= 0 and customerJ == -1):
+        if(customerI == 0):
+            routeSelected.addCustomer(j,demand[j],True)
+        else:
+            routeSelected.addCustomer(j,demand[j],False)
+    #customer j is served but i not.
+    if (customerI == -1 and customerJ >= 0):
+        if(customerJ == 0):
+            routeSelected.addCustomer(i,demand[i],True)
+        else:
+            routeSelected.addCustomer(i,demand[i],False)
+    
     return routes
 
 def FisherJaikumar_Kselector(graph,n_vehicles):
