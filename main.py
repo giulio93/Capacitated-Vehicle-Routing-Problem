447982d28528a72a0d31f1ff37f955d54afb8ae0
giulio93 <pilotto.giulio@gmail.com>
2019-08-22 14:40:56 +0200

    Genetic

--- main.py
+++ main.py
@@ -156,18 +156,18 @@ if __name__ == "__main__":
             if(sol.SearchaAndCompleteSequence(children,graphToSolve)):
               print("Invalid! " +str(fittingCrossover))
 
-          if (np.random.randint(1,100) <= mutationRate):
-            for route in mutantChild[1]:
-                c1 = np.random.randint(1,len(route.getCustomers())-1)
-                c2 = np.random.randint(1,len(route.getCustomers())-1)
-                route = sol.LocalSearch_FlippingPath(route,graphToSolve,c1,c2)
+          # if (np.random.randint(1,100) <= mutationRate):
+          #   for route in mutantChild[1]:
+          #       c1 = np.random.randint(1,len(route.getCustomers())-1)
+          #       c2 = np.random.randint(1,len(route.getCustomers())-1)
+          #       route = sol.LocalSearch_FlippingPath(route,graphToSolve,c1,c2)
 
-            fittingMutation = sum([m.getCost() for m in mutantChild[1]])
-            if(fittingMutation < mutantChild[0]):
-              popEra.append((fittingMutation,mutantChild[1]))
-              print("CROSSOVER + MUTATION HARD  ==> "+str(fittingMutation))
-            if(sol.SearchaAndCompleteSequence(children,graphToSolve)):
-              print("Invalid! " +str(fittingCrossover))
+          #   fittingMutation = sum([m.getCost() for m in mutantChild[1]])
+          #   if(fittingMutation < mutantChild[0]):
+          #     popEra.append((fittingMutation,mutantChild[1]))
+          #     print("CROSSOVER + MUTATION HARD  ==> "+str(fittingMutation))
+          #   if(sol.SearchaAndCompleteSequence(children,graphToSolve)):
+          #     print("Invalid! " +str(fittingCrossover))
 
         popEra.sort(key=lambda x:x[0],reverse=True)
         best = [popEra.pop() for i in range(int(len(popEra)/2))]
