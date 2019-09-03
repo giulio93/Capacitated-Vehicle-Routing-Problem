fcf287d260550eda3db0a1da9d8a8213225c7438
giulio93 <pilotto.giulio@gmail.com>
2019-09-03 08:39:46 +0200

    All

--- solver.py
+++ solver.py
@@ -46,6 +46,7 @@ def printResult(folderSol,folderRes):
         resume.write("============================== AUTO SAVE " +folderRes + " =============================" + "\n")
         #resume.write("Solution in "+folderRes + "\n")
         media = 0
+        timeMedio =0
         for sol in mysol:
             with open(path+'/'+sol, "r+") as f:
                 time = 0
@@ -65,14 +66,14 @@ def printResult(folderSol,folderRes):
                                             #print("Error of solution in "+sol + ": "+ str(float(error))+ "==> Stimated: " +str(stimated) + " ==> optimal: "+str(actual) + "==> Time: "+str(time))
                                             print(sol[4:-8] + " & "+ str(round(stimated,1)) + " & "+str(round(actual,1)) + " & " + str(float(round(error,4)))+ " & "+str(round(time,4)) +" "+ r" \\")
                                             media += (float(round(error,4)))
+                                            timeMedio += (round(time,4))
                                             f.write("Error of solution in "+sol + ": "+ str(float(error)))
                                             resume.write("Error of solution in "+sol + ": "+ str(float(error))+ "\n")
                     if(keywords.split(':')[0].strip()=="Time Horizon"):
                         time = float(keywords.split(':')[1].strip())
-        print("Media ==> "  + str(media /len(cvrp_sol)))
-                                
-                                    
-
+        print("Errore Media ==> "  + str(media /len(cvrp_sol)))
+        print("Tempo Medio ==> "  + str(timeMedio /len(cvrp_sol)))
+                                                                   
 def ClarkeWright(graph,parallel = True):
     print("Clarke & Wright have been evoked!")
 
